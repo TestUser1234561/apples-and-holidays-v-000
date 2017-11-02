d25c651b996d754ba4bd875d4e3fb0896567208b
Morgan Fudge <mfudge92@gmail.com>
2017-11-02 16:57:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, season|
     puts "#{key.capitalize}:"
     season.each do |key, items|
-      name = key.to_s.titleize 
+      name = key.to_s.titleiz
       puts "  #{name.gsub("_", " ").}: #{items.join(", ")}"
     end
   end
